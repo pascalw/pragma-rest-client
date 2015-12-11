25833ca026064267b9d96318bcffbce29808ab8f
Pascal Widdershoven <pascal@pwiddershoven.nl>
2015-12-11 20:10:43 +0100

    Simplify UI nesting

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
+import RequestsList from '../components/RequestsList';
 
 export default class App extends Component {
   static propTypes = {
@@ -10,7 +11,11 @@ export default class App extends Component {
     return (
       <div>
         <div id="header"><Link to="/">Moon</Link></div>
-        {this.props.children}
+        <div id="main">
+          <Link className="btn new-request" to='/new'>New request</Link>
+          <RequestsList/>
+          {this.props.children}
+        </div>
         {
           (() => {
             if (process.env.NODE_ENV !== 'production') {
