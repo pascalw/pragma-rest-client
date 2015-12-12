da1c8ad257ef7e186f078c640195f27839f8be22
Pascal Widdershoven <pascal@pwiddershoven.nl>
2015-12-13 00:17:03 +0100

    Support deleting of requests

--- app/reducers/requests.js
+++ app/reducers/requests.js
@@ -1,14 +1,26 @@
-import { ADD_REQUEST, UPDATE_REQUEST, RELOAD_ALL } from '../actions/request';
+import { ADD_REQUEST, UPDATE_REQUEST, DELETE_REQUEST, RELOAD_ALL } from '../actions/request';
+
+let findIndex = (state, request) => {
+  return state.findIndex((r) => r.id == request.id);
+};
 
 export default function requests(state = [], action) {
+  let idx = null;
+
   switch (action.type) {
     case ADD_REQUEST:
       return [...state, action.request];
     case UPDATE_REQUEST:
-      const idx = state.findIndex((r) => r.id == action.request.id);
+      idx = findIndex(state, action.request);
       return [
         ...state.slice(0, idx),
         action.request,
+        ...state.slice(idx + 1)
+      ];
+    case DELETE_REQUEST:
+      idx = findIndex(state, action.request);
+      return [
+        ...state.slice(0, idx),
         ...state.slice(idx + 1)
       ];
     case RELOAD_ALL:
