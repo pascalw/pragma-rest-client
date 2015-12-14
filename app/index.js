6a0127693c75e3552d227ab1eb2a2c0ddd9c7c26
Pascal Widdershoven <pascal@pwiddershoven.nl>
2015-12-14 20:17:05 +0100

    Add redux-simple-routing
    This way routing is also part of our state

--- app/index.js
+++ app/index.js
@@ -2,17 +2,23 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
+import { createHashHistory } from 'history';
+import { syncReduxAndRouter } from 'redux-simple-router';
 import routes from './routes';
 import configureStore from './store/configureStore';
 import Project from './project';
 import './app.scss';
 
 const store = configureStore();
+const history = createHashHistory();
+
+syncReduxAndRouter(history, store);
+
 new Project(store).keepInSync();
 
 render(
   <Provider store={store}>
-    <Router>
+    <Router history={history}>
       {routes}
     </Router>
   </Provider>,
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
+import { routeReducer } from 'redux-simple-router';
 import requests from './requests';
 import responses from './responses';
 
 const rootReducer = combineReducers({
   requests,
-  responses
+  responses,
+  routing: routeReducer
 });
 
 export default rootReducer;
