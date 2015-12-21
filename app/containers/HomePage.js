d7329526759c8388b76e92d648f80682fce3862d
Pascal Widdershoven <pascal@kabisa.nl>
2015-12-21 20:26:35 +0100

    Add opening projects

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
 import RequestsList from '../components/RequestsList';
 import RequestEditor from '../components/RequestEditor';
 
-export default class HomePage extends Component {
+export class HomePage extends Component {
   render() {
+    if (this.props.project == null)
+      return (<div><a href="#" onClick={ () => window.openProject() }>Open</a> a project to get started</div>);
+
     return (
       <div>Select a request on the left</div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    project: state.project
+  }
+}
+
+export default connect(mapStateToProps)(HomePage);
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import { routeReducer } from 'redux-simple-router';
 import requests from './requests';
 import responses from './responses';
+import project from './project';
 
 const rootReducer = combineReducers({
   requests,
   responses,
+  project,
   routing: routeReducer
 });
 
