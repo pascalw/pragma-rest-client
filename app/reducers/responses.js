c0e8f1a0ad82d2c684b7de16978ebfc4a9440b2c
Pascal Widdershoven <pascal@kabisa.nl>
2015-12-27 20:44:34 +0100

    Delete request responses on path updates

--- app/reducers/responses.js
+++ app/reducers/responses.js
@@ -1,10 +1,13 @@
 import { RECEIVE_RESPONSE } from '../actions/response';
+import { UPDATE_PATH } from 'redux-simple-router'
 import { Map } from 'immutable';
 
 export default function responses(state = new Map(), action) {
   switch (action.type) {
     case RECEIVE_RESPONSE:
       return state.updateIn([action.request.projectId, action.request.id], () => action.response);
+    case UPDATE_PATH:
+      return state.clear();
     default:
       return state;
   }
