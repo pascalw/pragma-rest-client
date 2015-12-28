6d370ffff9f1d1b11773345324d2a56ba0545675
Pascal Widdershoven <pascal@pwiddershoven.nl>
2015-12-28 20:37:12 +0100

    Add simple request loading UI indication

--- app/components/ResponseViewer.js
+++ app/components/ResponseViewer.js
@@ -1,13 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 
 class ResponseViewer extends Component {
-  static defaultProps = {
-    response: ''
-  };
-
   render() {
+    if (!this.props.response)
+      return <div/>;
+
+    if(this.props.response.pending) {
+      return <div>Loading...</div>;
+    }
+
     return (
-      <pre className="response">{ this.props.response.toString() } </pre>
+      <pre className="response">{ this.props.response.body.toString() } </pre>
     );
   }
 }
--- app/reducers/responses.js
+++ app/reducers/responses.js
@@ -1,11 +1,17 @@
-import { RECEIVE_RESPONSE } from '../actions/response';
+import { AWAITING_RESPONSE, RECEIVE_RESPONSE } from '../actions/response';
 import { UPDATE_PATH } from 'redux-simple-router'
 import { Map } from 'immutable';
 
 export default function responses(state = new Map(), action) {
   switch (action.type) {
+    case AWAITING_RESPONSE:
+      return state.updateIn([action.request.projectId, action.request.id], () => ({
+        pending: true
+      }));
     case RECEIVE_RESPONSE:
-      return state.updateIn([action.request.projectId, action.request.id], () => action.response);
+      return state.updateIn([action.request.projectId, action.request.id], () => ({
+        body: action.response
+      }));
     case UPDATE_PATH:
       return state.clear();
     default:
