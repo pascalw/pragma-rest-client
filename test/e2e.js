497a3ba4a1b04ef88423f6f4dbfa38371754f77c
Rui Salgado <ruivfsalgado@gmail.com>
2016-01-13 23:17:20 +0000

    Get simple e2e scenario working and add bridge to electron
    Scenario just selects a New Project from the native
    menu, fills in the form and checks that the project
    is listed.
    Injects a window.bridge in non-production environments
    to hold convenience methods to interact with 
    electron.
    In the actual test a handy executeFunction has your
    JS functions evaluated and executed in the test
    browser

--- test/e2e.js
+++ test/e2e.js
@@ -3,13 +3,16 @@ import chromedriver from 'chromedriver';
 import webdriver from 'selenium-webdriver';
 import { expect } from 'chai';
 import electronPath from 'electron-prebuilt';
-//import homeStyles from '../app/components/Home.module.css';
-//import counterStyles from '../app/components/Counter.module.css';
 
 chromedriver.start(); // on port 9515
 process.on('exit', chromedriver.stop);
 
 const delay = time => new Promise(resolve => setTimeout(resolve, time));
+
+const executeFunction = function (driver, func) {
+  const callable = "(" + func.toString() + ")()";
+  return driver.executeScript(callable);
+};
 
 describe('main window', function spec() {
   this.timeout(0);
@@ -32,101 +35,19 @@ describe('main window', function spec() {
     await this.driver.quit();
   });
 
-  xit('opens a new window', async () => {
-    const title = await this.driver.getTitle();
-    expect(title).to.equal('Gettable');
-
-    await delay(3000000);
-  });
-
   it('creates new project via menu', async () => {
-    await delay(3000);
+    executeFunction(this.driver, function () {
+      bridge.clickMenuEntry("File", "New project");
+    });
 
-    console.log("GONNA PRESS KEYS");
+    const inputs = await this.driver.findElements(webdriver.By.css('.new-project-page input'));
 
-    await this.driver.actions()
-      .keyDown(webdriver.Key.COMMAND)
-      .sendKeys('n')
-      .perform();
+    inputs[0].sendKeys('test name');
+    inputs[1].sendKeys('/tmp/test');
+    inputs[inputs.length - 1].click();
 
-    await delay(3000000);
+    const listedProjects = await this.driver.findElements(webdriver.By.css('.project-details .name'))
+
+    expect(await listedProjects[0].getText()).to.eql('test name');
   });
-
-  xit('opens existing project via menu', async () => {
-  });
-
-  //const findCounter = () => {
-  //  return this.driver.findElement(webdriver.By.className(counterStyles.counter));
-  //};
-  //
-  //const findButtons = () => {
-  //  return this.driver.findElements(webdriver.By.className(counterStyles.btn));
-  //};
-  //
-  //it('should open window', async () => {
-  //  const title = await this.driver.getTitle();
-  //  expect(title).to.equal('Hello Electron React!');
-  //});
-  //
-  //it('should to Counter with click "to Counter" link', async () => {
-  //  const link = await this.driver.findElement(webdriver.By.css(`.${homeStyles.container} > a`));
-  //  link.click();
-  //
-  //  const counter = await findCounter();
-  //  expect(await counter.getText()).to.equal('0');
-  //});
-  //
-  //it('should display updated count after increment button click', async () => {
-  //  const buttons = await findButtons();
-  //  buttons[0].click();
-  //
-  //  const counter = await findCounter();
-  //  expect(await counter.getText()).to.equal('1');
-  //});
-  //
-  //it('should display updated count after descrement button click', async () => {
-  //  const buttons = await findButtons();
-  //  const counter = await findCounter();
-  //
-  //  buttons[1].click();  // -
-  //
-  //  expect(await counter.getText()).to.equal('0');
-  //});
-  //
-  //it('shouldnt change if even and if odd button clicked', async () => {
-  //  const buttons = await findButtons();
-  //  const counter = await findCounter();
-  //  buttons[2].click();  // odd
-  //
-  //  expect(await counter.getText()).to.equal('0');
-  //});
-  //
-  //it('should change if odd and if odd button clicked', async () => {
-  //  const buttons = await findButtons();
-  //  const counter = await findCounter();
-  //
-  //  buttons[0].click();  // +
-  //  buttons[2].click();  // odd
-  //
-  //  expect(await counter.getText()).to.equal('2');
-  //});
-  //
-  //it('should change if async button clicked and a second later', async () => {
-  //  const buttons = await findButtons();
-  //  const counter = await findCounter();
-  //  buttons[3].click();  // async
-  //
-  //  expect(await counter.getText()).to.equal('2');
-  //
-  //  await this.driver.wait(() =>
-  //    counter.getText().then(text => text === '3' )
-  //  , 1000, 'count not as expected');
-  //});
-  //
-  //it('should back to home if back button clicked', async () => {
-  //  const link = await this.driver.findElement(webdriver.By.css(`.${counterStyles.backButton} > a`));
-  //  link.click();
-  //
-  //  await this.driver.findElement(webdriver.By.className(homeStyles.container));
-  //});
 });
